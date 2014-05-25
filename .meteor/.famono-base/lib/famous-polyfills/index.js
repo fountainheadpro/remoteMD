33abcf44aa8abde5da7afe2ea50ec6d643e3b052
Sergey Zelvenskiy <sergey@actions.im>
2014-05-25 10:02:41 -0700

    first ocmmmit

--- .meteor/.famono-base/lib/famous-polyfills/index.js
+++
@@ -1,5 +0,0 @@
-define('famous-polyfills/index', ["famous-polyfills/classList","famous-polyfills/functionPrototypeBind","famous-polyfills/requestAnimationFrame"], function(require, exports, module) {
-require('famous-polyfills/classList');
-require('famous-polyfills/functionPrototypeBind');
-require('famous-polyfills/requestAnimationFrame');
-});
